5381fe41559fb0a57a440ac34d17a928df591421
TheSweetPancake <76493138+TheSweetPancake@users.noreply.github.com>
2023-11-18 16:40:20 +0100

    Add files via upload

---
+++ iso_bc01/src/main/java/iso_bc01/Presentacion/IU_Evento.java
@@ -0,0 +1,36 @@
+package Diagrama_de_clases.Presentacion;
+
+public class IU_Evento {
+
+	/**
+	 * 
+	 * @param fecha
+	 * @param nombre
+	 * @param descripcion
+	 * @param particpantes
+	 */
+	public void añadirEvento(Date fecha, String nombre, String descripcion, int particpantes) {
+		// TODO - implement IU_Evento.añadirEvento
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void borrarEvento(int id) {
+		// TODO - implement IU_Evento.borrarEvento
+		throw new UnsupportedOperationException();
+	}
+
+	public void editarEvento() {
+		// TODO - implement IU_Evento.editarEvento
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarInfo() {
+		// TODO - implement IU_Evento.mostrarInfo
+		throw new UnsupportedOperationException();
+	}
+
+}
