03d5493a3bd54c5a74a550cb5a68d586de438227
TheSweetPancake <76493138+TheSweetPancake@users.noreply.github.com>
2023-11-27 14:57:12 +0100

    Add files via upload

---
+++ iso_bc01/src/main/java/iso_bc01/Presentacion/IU_Evento.java
@@ -0,0 +1,102 @@
+package iso_bc01.Presentacion;
+
+import Diagrama_de_clases.Dominio.*;
+
+public class IU_Evento {
+
+	/**
+	 * 
+	 * @param fecha
+	 * @param nombre
+	 * @param descripcion
+	 * @param particpantes
+	 */
+	public void anadirEvento(Date fecha, String nombre, String descripcion, int particpantes) {
+		// TODO - implement IU_Evento.anadirEvento
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void borrarEvento(int id) {
+		// TODO - implement IU_Evento.borrarEvento
+		throw new UnsupportedOperationException();
+	}
+
+	public void editarEvento() {
+		// TODO - implement IU_Evento.editarEvento
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarInfo() {
+		// TODO - implement IU_Evento.mostrarInfo
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param mensaje
+	 */
+	public void MostrarMensaje(String mensaje) {
+		// TODO - implement IU_Evento.MostrarMensaje
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param mensaje_err
+	 */
+	public void MostrarMensajeError(String mensaje_err) {
+		// TODO - implement IU_Evento.MostrarMensajeError
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param e
+	 */
+	public void click_abrir_evento(Evento e) {
+		// TODO - implement IU_Evento.click_abrir_evento
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param e
+	 */
+	public void click_cerrar_evento(Evento e) {
+		// TODO - implement IU_Evento.click_cerrar_evento
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param e
+	 */
+	public void click_consultar_eventos(Evento e) {
+		// TODO - implement IU_Evento.click_consultar_eventos
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Lista_de_eventos
+	 */
+	public void mostrarEvento(List<Evento> Lista_de_eventos) {
+		// TODO - implement IU_Evento.mostrarEvento
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param evento
+	 * @param asistente
+	 */
+	public void RegistrarseEvento(Evento evento, Asistente asistente) {
+		// TODO - implement IU_Evento.RegistrarseEvento
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ iso_bc01/src/main/java/iso_bc01/Presentacion/IU_Registrarse.java
@@ -0,0 +1,36 @@
+package iso_bc01.Presentacion;
+
+public class IU_Registrarse {
+
+	/**
+	 * 
+	 * @param u
+	 * @param correo
+	 * @param direccion
+	 */
+	public Asistente RegistrarseComoAsistente(usuario u, String correo, String direccion) {
+		// TODO - implement IU_Registrarse.RegistrarseComoAsistente
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param u
+	 * @param correo
+	 * @param sedeTrabajo
+	 */
+	public Personal RegistrarseComoPersonalDeAyD(usuario u, String correo, String sedeTrabajo) {
+		// TODO - implement IU_Registrarse.RegistrarseComoPersonalDeAyD
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param u
+	 */
+	public Organidador RegistraeseComoOrganizadorDeEventos(usuario u) {
+		// TODO - implement IU_Registrarse.RegistraeseComoOrganizadorDeEventos
+		throw new UnsupportedOperationException();
+	}
+
+}
